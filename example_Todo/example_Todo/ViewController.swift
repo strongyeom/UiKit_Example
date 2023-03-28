436de31c0400099848f9342a499ffca4b5ff1d22
yeomsungpil <present7256@gmail.com>
2023-03-28 11:37:12 +0900

    feat: UserDefaults 와 EditButton 추가

--- example_Todo/example_Todo/ViewController.swift
+++ example_Todo/example_Todo/ViewController.swift
@@ -10,15 +10,36 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var tasks: [Task] = []
+    @IBOutlet var editButton: UIBarButtonItem!
+    var doneButton: UIBarButtonItem?
+    
+    var tasks: [Task] = [] {
+        // 하나씩 배열안에 append 될때마다 didSet을 실행
+        didSet {
+            // 추가 될때마다 userDefaults에 저장됨
+            self.saveTasks()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTap))
         self.tableView.dataSource = self
+        self.tableView.delegate = self
+        // 데이터 로드
+        self.loadTasks()
+        
+    }
+    
+    @objc func doneButtonTap() {
+        self.navigationItem.leftBarButtonItem = self.editButton
+        self.tableView.setEditing(true, animated: true)
     }
 
     @IBAction func tabEditButton(_ sender: UIBarButtonItem) {
         
-        
+        guard !self.tasks.isEmpty else { return }
+        self.navigationItem.leftBarButtonItem = self.doneButton
+        self.tableView.setEditing(true, animated: true)
     }
     
     
@@ -47,6 +68,36 @@ class ViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
         
     }
+    
+    
+    func saveTasks() {
+        let data = self.tasks.map {
+            // map을 사용해서 딕셔너리 형태 만들기
+            [
+                "title": $0.title,
+                "done": $0.done
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        // userDefaults 저장
+        userDefaults.set(data, forKey: "tasks")
+    }
+    
+    // 할 일을 로드
+    func loadTasks() {
+        //userDefaults에 접근
+        let userDefaults = UserDefaults.standard
+        // userDefaults 불러오기
+        guard let data = userDefaults.object(forKey: "tasks") as? [[String: Any]] else { return }
+        // 딕셔너리를 다시 배열로 전환
+        self.tasks = data.compactMap{
+            guard let title = $0["title"] as? String else { return nil }
+            guard let done = $0["done"] as? Bool else { return nil }
+            return Task(title: title, done: done)
+        }
+        
+    }
+    
 }
 
 extension ViewController: UITableViewDataSource {
@@ -63,11 +114,53 @@ extension ViewController: UITableViewDataSource {
         // cell 위치 나타냄 [ 0 , tasks.count갯수만큼 ]
         let task = self.tasks[indexPath.row]
         cell.textLabel?.text = task.title
+        if task.done {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
+    }
+    
+    // cell이 테이블뷰에서 삭제, 스와이프 delete로 cell 삭제 가능
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        self.tasks.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        
+        if self.tasks.isEmpty {
+            self.doneButtonTap()
+        }
+    }
+    
+    // 편집모드에서 재정렬
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // 편집모드에서 재정렬
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var tasks = self.tasks
+        let task = tasks[sourceIndexPath.row]
+        tasks.remove(at: sourceIndexPath.row)
+        tasks.insert(task, at: destinationIndexPath.row)
+        self.tasks = tasks
     }
     
 }
 
+extension ViewController: UITableViewDelegate {
+    // 어떤 cell이 클릭이 되었는지
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var task = self.tasks[indexPath.row]
+        task.done = !task.done
+        self.tasks[indexPath.row] = task
+        // 선택된 cell만 reload
+        self.tableView.reloadRows(at: [indexPath], with: .automatic)
+        
+        
+        
+    }
+}
 
 
 
