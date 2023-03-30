99b6eb0539be362b6ce68251f3c269069b9e9a43
yeomsungpil <present7256@gmail.com>
2023-03-30 23:57:53 +0900

    feat: collectionView 연습

---
+++ example_AppStoreClone/example_AppStoreClone/PostCell.swift
@@ -0,0 +1,22 @@
+//
+//  PostCell.swift
+//  example_AppStoreClone
+//
+//  Created by 염성필 on 2023/03/30.
+//
+
+import UIKit
+
+class PostCell: UICollectionViewCell {
+    
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+    
+    override class func awakeFromNib() {
+    }
+    
+    
+    
+}
--- example_AppStoreClone/example_AppStoreClone/ViewController.swift
+++ example_AppStoreClone/example_AppStoreClone/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var images: [String] = ["one", "two", "three", "four"]
+    var titles = ["안녕하세요" , "여보세요" , "누구세요" , "컬렉션어렵네요"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostCell", for: indexPath) as! PostCell
+        cell.image.image = UIImage(named: images[indexPath.row])
+        cell.title.text = titles[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+}
