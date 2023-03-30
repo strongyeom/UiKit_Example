ee78572eb1f8a9ca5e4b61ede44e7cfd52bcbd21
yeomsungpil <present7256@gmail.com>
2023-03-30 13:18:30 +0900

    master: 앱 처음 시작

---
+++ example_AppStoreClone/example_AppStoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  example_AppStoreClone
+//
+//  Created by 염성필 on 2023/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
