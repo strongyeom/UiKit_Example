0f9d5b3a341359f7c11629fd4765376218fd7398
yeomsungpil <present7256@gmail.com>
2023-04-01 20:32:39 +0900

    feat: codebase

---
+++ example_iTunesAPI_codebase/example_iTunesAPI_codebase/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  example_iTunesAPI_codebase
+//
+//  Created by 염성필 on 2023/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var emptyView: UIView = {
+        var emptyLabel = UILabel()
+        
+    }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    
+
+
+}
+
