505abf49e5f7ac5825f65916c39cf3fad4a3e47a
yeomsungpil <present7256@gmail.com>
2023-04-08 20:13:34 +0900

    feat: image cache 준비중

---
+++ example_iTunesAPI_part2/example_iTunesAPI_part2/Model/Cache/ImageCacheManager.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCacheManager.swift
+//  example_iTunesAPI_part2
+//
+//  Created by 염성필 on 2023/04/08.
+//
+
+import Foundation
+import UIKit
+
+class ImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+    private init() { }
+}
