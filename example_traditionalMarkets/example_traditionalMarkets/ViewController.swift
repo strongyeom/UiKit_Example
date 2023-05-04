f50286082ec6761f740aa74095631ea79db5a90a
yeomsungpil <present7256@gmail.com>
2023-05-04 18:52:09 +0900

    add: 전통시장 API 구축 예정

---
+++ example_traditionalMarkets/example_traditionalMarkets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  example_traditionalMarkets
+//
+//  Created by 염성필 on 2023/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
